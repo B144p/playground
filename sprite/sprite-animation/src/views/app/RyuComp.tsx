2c243bf660efd4550949ff07ad9a0bffbf76a99a
BT_144p <gug321gug@gmail.com>
2023-11-29 23:28:37 +0700

    sprite-animate: animation using only css

---
+++ sprite/sprite-animation/src/views/app/RyuComp.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './css/ryuComp.css'
+
+type Props = {}
+
+const RyuComp: React.FC<Props> = () => {
+  return (
+    <div id="ryu" />
+  )
+}
+
+export default RyuComp
